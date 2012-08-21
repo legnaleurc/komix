908843fe5eec0bf104b6cd56db74ed6912d40ba4
Wei Cheng Pan <legnaleurc@gmail.com>
2012-08-21 20:56:54 +0800

    Remove dead code.

--- src/utility/imagewrapper.hpp
+++ src/utility/imagewrapper.hpp
@@ -1,31 +1,7 @@
 #ifndef KOMIX_UTILITY_IMAGE_HPP
 #define KOMIX_UTILITY_IMAGE_HPP
 
-//#include <QtCore/QString>
 #include <QtCore/QMetaType>
-/*
-#include <QtGui/QLabel>
-#include <QtGui/QGraphicsItem>
-
-#include <memory>
-
-namespace KomiX {
-
-	class ImageWrapper {
-	public:
-		ImageWrapper();
-		explicit ImageWrapper( QIODevice * device );
-
-		void moveToLabel( QLabel * label, const QSize & size ) const;
-		QGraphicsItem * takeGraphicsItem( QGraphicsItem * parent ) const;
-
-	private:
-		class Private;
-		std::shared_ptr< Private > p_;
-	};
-
-}
-*/
 
 Q_DECLARE_METATYPE( QIODevice * )
 
